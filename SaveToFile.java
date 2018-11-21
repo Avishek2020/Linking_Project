baa4fba1dad4636eb12684456b5c9482dc49345e
Avishek2020 <avishek2020@gmail.com>
2018-11-21 17:00:36 +0100

    Add files via upload

---
+++ SaveToFile.java
@@ -0,0 +1,18 @@
+package org.upb.musicclass;
+
+import org.aksw.limes.core.controller.ResultMappings;
+import org.aksw.limes.core.io.config.Configuration;
+import org.aksw.limes.core.io.serializer.ISerializer;
+import org.aksw.limes.core.io.serializer.SerializerFactory;
+
+public class SaveToFile {
+	 void Save(ResultMappings mappings, Configuration config) {
+	  String outputFormat = config.getOutputFormat();
+	  ISerializer output = SerializerFactory.createSerializer(outputFormat);
+	  output.setPrefixes(config.getPrefixes());
+	  output.writeToFile(mappings.getVerificationMapping(), config.getVerificationRelation(),
+	  config.getVerificationFile());
+	  output.writeToFile(mappings.getAcceptanceMapping(), config.getAcceptanceRelation(), config.getAcceptanceFile());
+	 }
+	
+}
